674411193b878e2aaaa8e88240b4fd4d670ba9a5
BitR <bitr@bitsaw.com>
2019-05-29 18:16:20 +0200

    Update Empyrion on start and redirect output to logfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,10 @@
 
 GAMEDIR="$HOME/Steam/steamapps/common/Empyrion - Dedicated Server/DedicatedServer"
 
-[ -d "$GAMEDIR" ] || {
-    echo "Bootstrapping game installation"
-
-    cd "$HOME"
-    [ "$BETA" ] && ./steamcmd.sh +login anonymous +app_update 530870 -beta experimental validate +quit
-    [ -z "$BETA" ] && ./steamcmd.sh +login anonymous +app_update 530870 validate +quit
-    mkdir "$GAMEDIR/Logs"
-}
+cd "$HOME"
+[ "$BETA" ] && ./steamcmd.sh +login anonymous +app_update 530870 -beta experimental +quit
+[ -z "$BETA" ] && ./steamcmd.sh +login anonymous +app_update 530870 +quit
+mkdir -p "$GAMEDIR/Logs"
 
 Xvfb :1 -screen 0 800x600x24 &
 export WINEDLLOVERRIDES="mscoree,mshtml="
@@ -17,4 +13,5 @@ export DISPLAY=:1
 
 cd "$GAMEDIR"
 
-/opt/wine-staging/bin/wine ./EmpyrionDedicated.exe -batchmode -logFile Logs/current.log "$@" # -dedicated "z:/server/Saves/Games/Game name/dedicated.yaml"
+tail -F Logs/current.log &
+/opt/wine-staging/bin/wine ./EmpyrionDedicated.exe -batchmode -logFile Logs/current.log "$@" &> $HOME/wine.log
